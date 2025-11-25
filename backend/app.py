9b716b50d985d398b7768ef34eadf27e6158dd74
Arian Nasr <81041177+arian-nasr@users.noreply.github.com>
2025-11-25 17:24:30 -0500

    tests

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,14 @@ from os import getenv
 from flask import Flask, render_template, request
 from pydantic import ValidationError
 from schemas import DayUpdatePayload
+from datetime import datetime
 
 MYSQL_HOST = getenv("MYSQL_HOST")
 MYSQL_USER = getenv("MYSQL_USER")
 MYSQL_PASSWORD = getenv("MYSQL_PASSWORD")
 MYSQL_DB = getenv("MYSQL_DB")
+
+print(datetime.now().isoformat())
 
 app = Flask(__name__)
 
